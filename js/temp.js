45865c0423b4aa78aacddb9179f1e454d7581882
smilejin92 <smilejin92@gmail.com>
2019-11-20 17:42:16 +0900

    edited

--- js/temp.js
+++ js/temp.js
@@ -1,7 +1,14 @@
-/* ------------------ CONSTANT ------------------ */
+/* ------------------------- CONSTANT ------------------------- */
 const URL = 'http://localhost:5000';
 
-/* ------------------ DOM ------------------ */
+// 각 난이도 별 포인트 및 제한시간(초)
+const EASY = [10, 30];
+const NORMAL = [25, 45];
+const HARD = [50, 60];
+const EXTREME = [100, 75];
+// const INIT_POINT = 100;
+
+/* ------------------------- DOM ------------------------- */
 const $ranking = document.querySelector('.ranking');
 const $currentPoint = document.querySelector('.current-point');
 const $bettingPoint = document.querySelector('.betting-point');
@@ -12,6 +19,7 @@ const $scoreList = document.querySelector('.score-list');
 const $selectError = document.querySelector('.select-error');
 const $scoreError = document.querySelector('.score-error');
 const $quizStart = document.querySelector('.quiz-start');
+const $quizRestart = document.querySelector('.quiz-restart');
 const $quizPrompt = document.querySelector('.quiz-prompt');
 const $quizWrapper = document.querySelector('.quiz-wrapper');
 const $choiceList = document.querySelector('.choice-list');
@@ -19,26 +27,28 @@ const $submit = document.querySelector('.submit');
 const $popup = document.querySelector('.popup');
 const $timer = document.querySelector('.timer');
 
-/* ------------------ State ------------------ */
-let category = ''; // 선택한 퀴즈 카테고리
+/* ------------------------- State ------------------------- */
+let currentPoint = 100; // 페이지 로드 시 보유 포인트를 200으로 설정
 let bettingPoint = 0; // 선택한 퀴즈 스코어
-let submittedAnswer = ''; // 선택한 답
-let quiz = {}; // 서버가 반환한 퀴즈 객체를 이 곳에 할당
-let currentPoint = 50; // 페이지 로드 시 보유 포인트를 200으로 설정
-let second = 30; // 풀이 제한 시간(초)
-let intervalId = 0; // 타이머를 실행시킨 테스크의 id
+let category; // 선택한 퀴즈 카테고리
+let quiz; // 서버가 반환한 퀴즈 객체를 이 곳에 할당
+let minute; // 풀이 제한 시간(분)
+let second; // 풀이 제한 시간(초)
+let intervalId; // 타이머를 실행시킨 테스크의 id
+let submittedAnswer; // 선택한 답
 // let isPlaying = false; // 현재 퀴즈가 진행 중인지
 
 // 문제 목록
-let problems = []; // 전체 문제
-let htmlProblems = []; // html 문제
-let cssProblems = []; // css 문제
-let jsProblems = []; // js 문제
+let problems; // 전체 문제
+let htmlProblems; // html 문제
+let cssProblems; // css 문제
+let jsProblems; // js 문제
 
 // 랭킹 목록
-let ranking = [];
+let ranking;
 
-/* ------------------ Function ------------------ */
+/* ------------------------- Function ------------------------- */
+
 // 스크롤을 최하단으로 위치시킨다.
 const scrollDown = () => {
   window.scrollTo({
@@ -49,12 +59,19 @@ const scrollDown = () => {
   });
 };
 
+const getMaxUserId = () => Math.max(0, ...ranking.map(user => user.id)) + 1;
+
 // 랭킹 정보를 명예의 전당에 표시한다.
-const renderRanking = () => {
+const renderScoreBoard = () => {
   // console.log(ranking);
   ranking.sort((user1, user2) => user2.score - user1.score);
 
-  let html = '';
+  let html = `<tr class="row">
+    <th>Rank</th>
+    <th>Username</th>
+    <th>Score</th>
+  </tr>`;
+
   ranking.forEach((user, idx) => {
     html += `<tr class="row user">
     <td class="rank">${idx + 1}</td>
@@ -62,7 +79,7 @@ const renderRanking = () => {
     <td class="score">${user.score}</td>
     </tr>`;
   });
-  $ranking.innerHTML += html;
+  $ranking.innerHTML = html;
 };
 
 // 보유 포인트/베팅 포인트를 스코어 보드에 표시한다.
@@ -72,6 +89,22 @@ const renderPoint = () => {
   bettingPoint = evalPoint ? 0 : bettingPoint;
   $bettingPoint.textContent = evalPoint ? 0 : bettingPoint;
   $currentPoint.textContent = evalPoint ? currentPoint : currentPoint - bettingPoint;
+};
+
+// DOM의 classList에 className을 추가한다.
+const addClass = (className, ...domArr) => {
+  // console.log(domArr);
+  domArr.forEach(dom => {
+    dom.classList.add(className);
+  });
+};
+
+// DOM의 classList에 className을 삭제한다.
+const removeClass = (className, ...domArr) => {
+  // console.log(domArr);
+  domArr.forEach(dom => {
+    dom.classList.remove(className);
+  });
 };
 
 // 정답 제출 후 팝업을 표시한다. - 리팩토링 가능한지?
@@ -89,7 +122,8 @@ const popup = type => {
   } else if (type === 'wrong') {
     html = `<p>오답입니다.<br>
       ${bettingPoint}포인트를 차감합니다.<br>
-      계속 진행하시겠습니까?</p>
+      계속 진행하시겠습니까?<br>
+      정답: ${quiz.answer}</p>
       <div class="btn-group">
         <button class="continue">YES</button>
         <button class="quit">NO</button>
@@ -98,11 +132,13 @@ const popup = type => {
   } else if (type === 'allin') {
     html = `<p>오답입니다.<br>
       포인트를 모두 소진하였습니다.<br>
-      분발하세요.</p>
+      분발하세요.<br>
+      정답: ${quiz.answer}</p>
       <div class="btn-group">
         <button class="allin">OK</button>
       </div>`;
   } else if (type === 'congrat') {
+    addClass('disable', $quizStart);
     html = `<p> 축하합니다! 모든 문제를 완료했습니다.</br>
       최종 포인트는 ${currentPoint}포인트 입니다.</br>
       명예의 전당에 등록하시겠습니까?</p>
@@ -118,7 +154,8 @@ const popup = type => {
         <button class="honor-quit">NO</button>
       </div>`;
   } else {
-    html = `<p>명예의 전당에 등록할 닉네임을 입력해주세요.</p>
+    html = `<p>명예의 전당에 등록할 닉네임을 입력 후</br>
+      엔터 키를 눌러주세요.</p>
       <div class="ranker">
         <input type="text" class="ranker-name">
       </div>`;
@@ -200,14 +237,18 @@ const formatText = p => {
 
 // 타이머에 시간을 표시한다.
 const displayTime = () => {
-  $timer.textContent = `00:${(second + '').length > 1 ? second : '0' + second}`;
+  // console.log(minute, second);
+  $timer.textContent = `
+  ${(minute + '').length > 1 ? minute : '0' + minute}:
+  ${(second + '').length > 1 ? second : '0' + second}`;
 };
 
 // 타이머를 실행한다.
-const decreaseSecond = () => {
-  second -= 1;
-
-  displayTime();
+const runTimer = () => {
+  if (second === 0 && minute > 0) {
+    minute -= 1;
+    second = 60;
+  }
 
   if (!second) {
     bettingPoint = 0;
@@ -216,6 +257,9 @@ const decreaseSecond = () => {
     clearInterval(intervalId);
     popup(!currentPoint ? 'allin' : 'wrong');
     renderPoint();
+  } else {
+    second -= 1;
+    displayTime();
   }
 };
 
@@ -227,7 +271,15 @@ const renderQuiz = () => {
   const random = Math.floor(Math.random() * problem.length);
 
   quiz = problem[random];
-  second = 30;
+
+  // 포인트별 제한 시간 설정
+  if (quiz.point === EASY[0]) [, second] = EASY;
+  else if (quiz.point === NORMAL[0]) [, second] = NORMAL;
+  else if (quiz.point === HARD[0]) [, second] = HARD;
+  else [, second] = EXTREME;
+
+  minute = Math.floor(second / 60);
+  second %= 60;
 
   const formattedQuestion = formatText(quiz.question);
   const formattedDescription = formatText(quiz.description);
@@ -247,10 +299,10 @@ const renderQuiz = () => {
   });
 
   $choiceList.innerHTML = choiceList;
-  $timer.textContent = `00:${second}`;
-
   $quizPrompt.classList.remove('hide');
-  intervalId = setInterval(decreaseSecond, 1000);
+
+  displayTime();
+  intervalId = setInterval(runTimer, 100);
   scrollDown();
 };
 
@@ -264,29 +316,17 @@ const getAnswer = () => {
   return res;
 };
 
-// DOM의 classList에 className을 추가한다.
-const addClass = (className, ...domArr) => {
-  // console.log(domArr);
-  domArr.forEach(dom => {
-    dom.classList.add(className);
-  });
-};
+/* ------------------------- Event Binding ------------------------- */
 
-// DOM의 classList에 className을 삭제한다.
-const removeClass = (className, ...domArr) => {
-  // console.log(domArr);
-  domArr.forEach(dom => {
-    dom.classList.remove(className);
-  });
-};
-
-/* ------------------ Event Binding ------------------ */
-// 페이지 로드 시 서버에 데이터를 요청하여 응답 받은 데이터를 상태에 저장한 후 브라우저에 표시한다.
+// 페이지 로드 시 서버에 데이터를 요청하여
+// 응답 받은 데이터를 상태에 저장한 후
+// 브라우저에 표시한다.
 window.onload = async () => {
   try {
     // 명예의 전당 정보 요청 및 표시
     ranking = await fetch(`${URL}/ranking`).then(rank => rank.json());
-    renderRanking();
+    console.log(ranking);
+    renderScoreBoard();
 
     // 문제 목록 요청 및 표시
     // 문제를 카테고리 별로 분류
@@ -312,7 +352,7 @@ $categoryList.onchange = ({ target }) => {
   bettingPoint = 0;
 
   removeClass('hide', $quizStart);
-  addClass('hide', $scoreError);
+  addClass('hide', $scoreError, $selectError);
   renderPoint();
   renderScore(category);
 };
@@ -322,6 +362,7 @@ $scoreList.onchange = ({ target }) => {
   if (target.classList.contains('score')) return;
 
   bettingPoint = +flipCard(target);
+  addClass('hide', $selectError);
 
   if (currentPoint < bettingPoint) {
     addClass('hide', $quizStart);
@@ -385,12 +426,63 @@ $popup.onclick = ({ target }) => {
   } else if (btn.contains('quit')) { // 종료할 경우 (currentPoint > 0)
     popup('honor');
   } else if (btn.contains('allin')) { // 종료할 경우 (currentPoint === 0)
-    window.location.reload();
+    // 방법 1. 페이지를 리로드한다.
+    // window.location.reload();
+
+
+    // 방법 2. restart 버튼을 표시한다.
+    // - 팝업을 없앤다.
+    // - 카테고리 선택 영역과 스코어 선택 영역을 숨김 처리한다.
+    // - start 버튼을 restart 버튼으로 대체한다.
+    addClass('hide', $popup, $quizCategory, $quizScore, $quizStart, $quizPrompt);
+    removeClass('hide', $quizRestart);
+    currentPoint = 0;
+    renderPoint();
+    // - restart 버튼을 클릭하면
+    //   - 모든 상태를 초기화한다.
   } else if (btn.contains('honor-continue')) { // 종료 후 명예의 전당에 등록할 경우
-    console.log('honor-continue');
     popup('ranker');
   } else if (btn.contains('honor-quit')) { // 종료 후 명예의 전당에 등록하지 않을 경우
-    console.log('honor-quit');
-    window.location.reload();
+    // 방법 1. 페이지를 리로드한다.
+    // window.location.reload();
+
+
+    // 방법 2. restart 버튼을 표시한다.
+    // - 팝업을 없앤다.
+    // - 카테고리 선택 영역과 스코어 선택 영역을 숨김 처리한다.
+    // - start 버튼을 restart 버튼으로 대체한다.
+    addClass('hide', $popup, $quizCategory, $quizScore, $quizStart, $quizPrompt);
+    removeClass('hide', $quizRestart);
+    // - restart 버튼을 클릭하면
+    //   - 모든 상태를 초기화한다.
+    currentPoint = 0;
+    renderPoint();
   }
+};
+
+// db.json에 ranking 테이블에 username, score를 추가한다.
+$popup.onkeyup = async ({ target, keyCode }) => {
+  const username = target.value.trim();
+  if (!username || keyCode !== 13) return;
+  // console.log(username);
+  try {
+    await fetch(`${URL}/ranking`, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({ id: getMaxUserId(), username, score: currentPoint })
+    });
+    ranking = await fetch(`${URL}/ranking`).then(res => res.json());
+    console.log(ranking);
+    // window.location.reload();
+    renderScoreBoard();
+    // 숨길거 숨기고 보일거 보이고
+    // restart 버튼
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// restart 버튼을 클릭하면 페이지를 리로드한다.
+$quizRestart.onclick = () => {
+  window.location.reload();
 };
